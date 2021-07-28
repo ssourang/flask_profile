c8d02c6bef66ae27571c480046b248be22727b29
Serigne Sourang <ssmsourang1@gmail.com>
2021-07-28 09:36:57 -0400

    reformatted working directory

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-print('hello')
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
